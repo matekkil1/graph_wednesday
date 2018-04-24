63d19947f72f8d89a85ba089d8285c6bfffdd39d
matekkil1 <you@example.com>
2018-04-24 21:31:57 +0200

    dodanie funkcji do grafu

--- grafy_windows/include/Graph.h
+++ grafy_windows/include/Graph.h
@@ -1,15 +1,17 @@
 #ifndef GRAPH_H
 #define GRAPH_H
 #include <limits.h>
+#include <iomanip>
+#include <stack>
 
-template < class T, int N, int M> // N-number o vertexes M-number of edges
+template < class T, int N, int M> // N-number of vertexes, M-number of edges
 class Graph
 {
     int inc_mat[N][M] ={};      //incidential matrix with elements equal zero
     T vertex_val[N];        //each vertex value
     bool vertex_check[N];   //if a vertex was checked by DFS algorythm
+    bool is_vertex_checked[N]; //if given vertex is checked
     public:
-       // template < class T, int N>
         bool VerAdd()
         {   for(int i=0;i<N;i++)
             {
@@ -17,7 +19,9 @@ class Graph
                  std::cin>> ran;
                  vertex_val[i]=ran;
             }
+            return true;
         }
+
         bool EdgeAdd()
         {int a,b;
          for(int c=0;c<M;c++)
@@ -57,13 +61,62 @@ class Graph
         }
 
 
-        bool IncMatDis();
-        bool ifGraphChecked();
-        bool DisplayVer()
-
-        {
-            std::cout<<N;
+        bool IncMatDis()
+        { std::cout << "   ";
+          for(int i = 0; i < M; i++) std::cout << std::setw(3) << i;
+          std::cout << std::endl << std::endl;
+          for(int i = 0; i < N; i++)
+          {
+            std::cout << std::setw(3) << i;
+            for(int j = 0; j < M; j++) std::cout << std::setw(3) << (int) inc_mat[i][j];
+            std::cout << std::endl;
+          }
+        return true;
         }
+
+        bool isGraphChecked();
+        bool DisplayVer()
+        {
+            for(int i=0;i<N;i++)
+            {
+                std::cout<<"Dla wierzcholka o indeksie "<<i<<" wartosc wynosi: "<< vertex_val[i]<<std::endl;
+            }
+
+        }
+
+       /* bool DSF()
+        {   int currver=0; //index of current vertex
+            bool v_is_checked; //is current vertex checked
+            int ver_to_start = 0; // index of the vertex to start searching
+            if(inc_mat[][ver_to_start])
+        while()
+
+
+        } */
+
+       /* void DFS(int v)
+        {
+        int i,j;
+
+        visited[v] = true;     // Zaznaczamy wêze³ jako odwiedzony
+        cout << setw(3) << v;  // Przetwarzamy wêze³ (wypisujemy jego numer)
+
+            // Rekurencyjnie odwiedzamy nieodwiedzonych s¹siadów
+
+        for(i = 0; i < m; i++)
+        if(A[v][i] == 1)
+            for(j = 0; j < n; j++)
+            if(A[j][i] == -1)
+            {
+                if(!visited[j]) DFS(j);
+                break;
+            }
+        }*/
+
+
+
+
+
     protected:
 
     private:
--- grafy_windows/main.cpp
+++ grafy_windows/main.cpp
@@ -5,8 +5,11 @@ using namespace std;
 
 int main()
 {   int * sth;
-    Graph < int,5,5> graph;
+    Graph < int,3,4> graph;
     graph.VerAdd();
     graph.EdgeAdd();
+    graph.IncMatDis();
+    graph.DisplayVer();
+  ;
     return 0;
 }
